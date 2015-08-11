74c100e087891b68f56f39beed1f3d57d8918ea5
Mat Gadd <source@catchall.drarok.com>
2015-08-11 13:38:19 +0100

    Initial project setup

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp');
+
+var jasmine = require('gulp-jasmine');
+var jscs = require('gulp-jscs');
+var jshint = require('gulp-jshint');
+
+var source = [
+  'gulpfile.js',
+  'lib/**/*.js',
+  'spec/**/*.js'
+];
+
+gulp.task('jscs', function () {
+  return gulp.src(source)
+    .pipe(jscs());
+});
+
+gulp.task('jshint', function () {
+  return gulp.src(source)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'))
+    .pipe(jshint.reporter('fail'));
+});
+
+gulp.task('jasmine', function () {
+  var specs = ['spec/**/*.spec.js'];
+  return gulp.src(specs)
+    .pipe(jasmine({
+      includeStackTrace: true,
+      verbose: true
+    }));
+});
+
+gulp.task('standards', ['jshint', 'jscs']);
+gulp.task('test', ['jasmine']);
+gulp.task('default', ['jasmine', 'jshint', 'jscs']);
